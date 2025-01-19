a4017b82c33738d8be670f5cd2385d167f0bcdc5
amin <sayedamin01@gmail.com>
2025-01-19 13:24:46 +0530

    edit in link

--- src/dev-server-link.jsx
+++ src/dev-server-link.jsx
@@ -1,1 +1,1 @@
-export const SERVER_LINK = import.meta.env.VITE_REACT_APP_SERVER_LINK || "http://localhost:8080";
+export const SERVER_LINK = import.meta.env.VITE_REACT_APP_SERVER_LINK || "http://13.60.50.12:8080";
