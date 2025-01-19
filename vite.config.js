c12837171c887fd47377a545cd120f8269f3befb
amin <sayedamin01@gmail.com>
2025-01-19 08:52:05 +0530

    vite configuration for build changes

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,21 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['ace-builds', 'uuid'], 
+    include: ['ace-builds', 'uuid', 'socket.io-client'], // Add socket.io-client here
   },
   
-  build: {
+  build: { 
+    chunkSizeWarningLimit: 1000, 
     sourcemap: true, 
     rollupOptions: {
       output: {
         manualChunks: {
-          ace: ['ace-builds'],
+          ace: ['ace-builds', 'socket.io-client'], // Bundle socket.io-client with ace-builds
         },
       },
-      external: ['uuid'], 
+      external: ['uuid'], // uuid will be external
     },
-    target: 'esnext', // Use the latest version of JavaScript that supports top-level await
+    target: 'esnext', // Use the latest version of JavaScript
   },
 
   assetsInclude: ['**/*.woff2', '**/*.otf'], // Include font files for bundling
